1bc33bc600af8f6bd979b1a4c5c9cfdb18ffd5c3
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-04-30 00:43:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const openBtn = document.getElementById("openModal")
 const closeBtn = document.querySelector(".close-modal")
 
 openBtn.onclick = () => {
-	modal.style.display = "block";
+	modal.classList.add("show");
 }
 
 closeBtn.onclick = () => {
-	modal.style.display = "none";
+	modal.classList.remove("show");
 }
 
 window.onclick = (e) => {
 	if(e.target === modal) {
-		modal.style.display = "none";
+		modal.classList.remove("show");
 	}
 }
